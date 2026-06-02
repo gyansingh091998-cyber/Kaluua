6dc76f171e229cdbc78cb696d5bb78b015d3045e
gyansingh091998-cyber <gyansingh091998@gmail.com>
2026-06-02 23:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,6 +147,5 @@ def send():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
